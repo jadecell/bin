17126635c7aee1d61230e1af2f5e6606115849f8
Jackson McCrory <jackson@mccrory.xyz>
2025-05-28 20:55:12 -0700

    initial commit

---
+++ packages.nix
@@ -0,0 +1,31 @@
+{
+  perSystem =
+    { pkgs, lib, ... }:
+    let
+      bashDeps = rec {
+        extract = with pkgs; [
+          gnutar # tar
+          xz # unlzma, unxz
+          bzip2 # bunzip2
+          unar # .rar
+          gzip # gunzip, uncompress
+          p7zip # .7z
+          cabextract # cabextract
+          unzip
+          gnused
+          zstd
+        ];
+
+      };
+    in
+    {
+      packages = lib.attrsets.mapAttrs (
+        name: path:
+        pkgs.writeShellApplication {
+          name = "${name}";
+          runtimeInputs = path;
+          text = (builtins.readFile ./src/${name});
+        }
+      ) bashDeps;
+    };
+}
